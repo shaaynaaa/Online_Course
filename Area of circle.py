6145c05f795c71edd43d6b1e60a089f91bd5b76d
shaaynaaa <shaaynaaa0.0@gmail.com>
2026-06-02 02:36:08 +0530

    Add files via upload

---
+++ Area of circle.py
@@ -0,0 +1,7 @@
+#ASSIGNMENT 2
+d= float(input('Measure of diameter: '))
+# str for string bool for boolean
+r= d/2
+ar= 3.14 * r**2
+print('radius of circle is', r)
+print("Area of circle: " ,ar)
---
+++ Repetetion.py
@@ -0,0 +1,10 @@
+#ASSIGNMENT 1
+for i in range(5):
+    print('shayna')
+print("OR")
+print(('shayna' + "\n") *5);
+#here \n means next line to print shayna bar bar diff lines mei 
+# \n ko string mei likhe ge with '..'
+# aur +,-,*,/ ye arithemetic operators string ko string se krte h aur int ko int se 
+#hence agar \n with '..' hota toh error aata 
+# if we put comma instead of plus vo next line nhi jayega
